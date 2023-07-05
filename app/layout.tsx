e8451f6b8eef6ea037f76e7516a91c8ad8a9552e
rdcox <ryancox101@gmail.com>
2023-07-05 19:53:44 -0400

    fix white bar at the top

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <nav className="font-mono flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white shadow sm:items-baseline w-full">
+        <nav className="font-mono flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 shadow sm:items-baseline w-full">
           <div className="mb-2 sm:mb-0">
             <a href="/" className="text-2xl no-underline text-grey-darkest hover:text-blue-dark">rdcox</a>
           </div>
--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '*'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
