b59276593aef02cd07805d1df26c6a898965c727
rdcox <ryancox101@gmail.com>
2023-07-08 18:25:51 -0400

    attempts to fix navbar have been futile, restoring original

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <NavBar></NavBar>       
+        <nav className="font-mono flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 shadow sm:items-baseline w-full">
+          <div className="mb-2 sm:mb-0">
+              <a href="/" className="text-2xl no-underline text-grey-darkest hover:text-blue-dark">rdcox</a>
+          </div>
+          <div>
+              <a href="/resume" className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">resume</a>
+          </div>
+        </nav>     
         <main className="min-h-screen bg-neutral-900 flex flex-col items-center">
           {children}
         </main>
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,10 +1,12 @@
-import Link from "next/link";
-
 export default function NavBar() {
     return (
-    <nav className="flex flex-row justify-between py-4 px-6 items-baseline w-full">
-        <Link href="/" className="text-2xl no-underline text-grey-darkest">rdcox</Link>
-        <Link href="/resume" className="text-lg no-underline text-grey-darkest ml-2">resume</Link>
-      </nav>
+    <nav className="font-mono flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 shadow sm:items-baseline w-full">
+        <div className="mb-2 sm:mb-0">
+            <a href="/" className="text-2xl no-underline text-grey-darkest hover:text-blue-dark">rdcox</a>
+        </div>
+        <div>
+            <a href="/resume" className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">resume</a>
+        </div>
+    </nav>
     )
 }
