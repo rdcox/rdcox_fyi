11b7d9d0527360d06931ff7b0b12fcb2e0a847ba
rdcox <ryancox101@gmail.com>
2023-07-08 17:06:45 -0400

    removing junk from resumedoc component

--- components/ResumeDoc.tsx
+++ components/ResumeDoc.tsx
@@ -1,5 +1,4 @@
 import { Page, View, Document, StyleSheet, pdf } from '@react-pdf/renderer';
-import { getResumeData } from '@/lib/resume-data';
 
 const styles = StyleSheet.create({
     resume: {
@@ -52,36 +51,11 @@ const styles = StyleSheet.create({
   });
 
   export default async function ResumeDoc() {
-    const { about, contact, professionalExperience, education, skills } = await getResumeData();
-
     return (
         <Document>
             <Page size="A4" style={styles.resume}>
                 <View>
-                <div className="resume">
-      
-      <div className="header">
-        <h1>{about.name}</h1>
-        <p>{about.tag} | {about.subTag}</p>
-        <ul className="contact-info">
-          {contact?.map((inf: any) => (
-            <li>{String(inf.value).includes("http") ? <a href={inf.value} target="_blank">{inf.name}</a> : inf.value}</li>
-          ))}
-        </ul>
-      </div>
-
-      <div className="about-me">
-        <h2>About Me</h2>
-        <p>{about.aboutMe}</p>
-      </div>
-      
-      <h2>Skills</h2>
-      <ul>
-        {skills?.length ? skills.map((skill: any) => (
-          <li>{skill.name}</li>
-          )) : <p>{"No skills :("}</p>}
-      </ul>
-    </div>
+                
                 </View>
             </Page>
         </Document>
