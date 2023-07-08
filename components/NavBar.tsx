bda56dd1da8ef7178303763010008ba31f38ec36
rdcox <ryancox101@gmail.com>
2023-07-08 17:43:37 -0400

    simplify css and fix navbar styling

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 export default function NavBar() {
     return (
     <nav className="font-mono flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 shadow sm:items-baseline w-full">
         <div className="mb-2 sm:mb-0">
-            <a href="/" className="text-2xl no-underline text-grey-darkest hover:text-blue-dark">rdcox</a>
+          <Link href="/" className="text-2xl no-underline text-grey-darkest hover:text-blue-dark">rdcox</Link>
         </div>
         <div>
-            <a href="/resume" className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">resume</a>
+        <Link href="/resume" className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">resume</Link>
         </div>
-    </nav>
+      </nav>
     )
 }
