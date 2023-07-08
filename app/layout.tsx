bdafce57fb32183acd939261a265278e1eaa82c8
rdcox <ryancox101@gmail.com>
2023-07-08 18:28:05 -0400

    compare navbars side by side

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <NavBar></NavBar>
         <nav className="font-mono flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 shadow sm:items-baseline w-full">
           <div className="mb-2 sm:mb-0">
               <a href="/" className="text-2xl no-underline text-grey-darkest hover:text-blue-dark">rdcox</a>
